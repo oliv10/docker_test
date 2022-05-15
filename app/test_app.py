2f98b9a080dfe377fd839e437339ee3bc3a37613
Oliver Scotten <revi99@me.com>
2022-05-15 04:47:56 +0000

    Did a thing

--- app/test_app.py
+++ app/test_app.py
@@ -1,15 +1,12 @@
 import unittest
-from app import *
+from app import app as tapp
 
-class test_app(unittest.TestCase):
+class app(unittest.TestCase):
         
     def test_getName(self):
-        test = app("Name", 56)
+        test = tapp("Name", 56)
         self.assertTrue(test.getName() == "Name")
 
     def test_getAge(self):
-        test = app("Name", 56)
+        test = tapp("Name", 56)
         self.assertTrue(test.getAge() == 56)
-
-if __name__ == '__main__':
-    unittest.main()
